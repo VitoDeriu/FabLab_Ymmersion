c38c7cf10e2b2355033a757e18c521770017837c
Arthur Herry <arthur.herry@ynov.com>
2025-02-19 16:25:58 +0100

    ajout css login et correction de bugs

--- frontend/.next/types/app/page.ts
+++ frontend/.next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\alexa\OneDrive\Bureau\ymmersion\FabLab_Ymmersion\frontend\app\page.js
+// File: C:\Users\Arthur\Desktop\fabla\FabLab_Ymmersion\frontend\app\page.js
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
