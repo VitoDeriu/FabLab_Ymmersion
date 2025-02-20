ecc156094d041fb60b537dda131d613ba633e900
Arthur Herry <arthur.herry@ynov.com>
2025-02-20 14:48:30 +0100

    ajout du contenu de la page machines

--- front/src/routes/router.js
+++ front/src/routes/router.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const HomeController = require('../controller/homeController');
+const MachinesController = require('../controller/machinesController');
 
 router.get('/', HomeController.Index);
+router.get('/machines', MachinesController.Index);
 
 module.exports = router;
